de248eb7b14339d7f00498bb67c2285cda03e8d1
Khoa, Vu <khoavu.its@gmail.com>
2023-12-28 01:33:12 +0700

    init

---
+++ project/src/main/java/com/kris/exercise6/model/Movie.java
@@ -0,0 +1,37 @@
+package com.kris.exercise6.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Movie {
+	private final String title;
+	private final int releaseYear;
+	
+	private Set<Actor> actors = new HashSet<>();
+	
+	public Movie(String title, int releaseYear) {
+		this.title = title;
+		this.releaseYear = releaseYear;
+	}
+	
+	public String title() {
+		return this.title;
+	}
+	
+	public int releaseYear() {
+		return this.releaseYear;
+	}
+	
+	public void addActor(Actor actor) {
+		this.actors.add(actor);
+	}
+	
+	public Set<Actor> actors() {
+		return this.actors;
+	}
+	
+	@Override
+	public String toString() {
+		return "Movie{" + "title=" + title + ", releaseYear=" + releaseYear + ", actors=" + actors + '}';
+	}
+}
